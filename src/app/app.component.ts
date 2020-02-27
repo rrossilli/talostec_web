9ebfe9a42c3c6bd56fee754b7d41098d618d0615
Robert Rossilli <rrossilli55@gmail.com>
2020-02-27 14:17:51 -0500

    simple changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-declare var $:any;
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'talostec-web';
+export class AppComponent implements OnInit  {
+
+  email = "rob@talostec.io"
+  title = 'talostec';
+
+  constructor(){
+  }
+
+  ngOnInit() {
+  }
+
+  showContactInfo(){
+    window.location.href = "mailto:" + this.email;
+  }
+
 }
